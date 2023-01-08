2ad4d6da2bb0d5540aaafce7de00fece31fa25ed
farazahmed2048 <107956730+farazahmed2048@users.noreply.github.com>
2023-01-08 23:05:36 +0500

    Add files via upload

---
+++ Web_LAB#2/script.js
@@ -0,0 +1,9 @@
+function Ok(){
+    alert("Ok is Clicked")
+}
+function Cancel(){
+    alert("Cancel is Clicked")
+}
+function login(){
+    alert("show message in an alert as Logged in successfully ")
+}
---
+++ Web_LAB#2/script1.js
@@ -0,0 +1,6 @@
+function Ok(){
+    alert("Ok is Clicked")
+}
+function Cancel(){
+    alert("Cancel is Clicked")
+}
